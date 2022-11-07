8ff0d77ee6d6b2a94a933de079f96ecd1e689a74
David Mackey <davidmackey92@gmail.com>
2022-11-06 21:09:40 -0600

    organized starter code into appropriate folder structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  var userInput = window.prompt("How long do you want your password to be?")
+
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength)) {
+    window.alert("That's not a number!")
+    return
+  } else {
+    window.alert("That is a valid number!")
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
